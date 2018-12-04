322ac23d6d2b2f2acc41dccd3f3a5c9dc5d6ea26
Marcelo <marce.8706@gmail.com>
2018-12-04 16:01:45 -0300

    se agrego funciones y regiter

---
+++ funciones/funcionestp.php
@@ -0,0 +1,23 @@
+<?php
+function validacion(){
+$errores=[];
+  if ($_POST){
+    $hash=password_hash($_POST["password"], PASSWORD_DEFAULT);
+    if (strlen($_POST["nombre"])==0) {
+      $errores[]= "No llenaste el nombre <br>";
+    }
+    if (filter_var($_POST["email"],FILTER_VALIDATE_EMAIL)==false) {
+      $errores[]= "El mail no tiene el formato correcto <br>";
+    }
+    if (is_numeric($_POST["edad"])==false) {
+      $errores[]= "La edad debe ser numerica <br>";
+    }
+
+    }
+
+    return $errores;
+
+
+
+}
+ ?>
