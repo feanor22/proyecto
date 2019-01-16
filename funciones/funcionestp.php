9cab769cee7399331b9d83393ecc746579c363fd
marcelo <marce.8706@gmail.com>
2019-01-15 22:01:07 -0300

    se agregan validaciones y

--- funciones/funcionestp.php
+++ funciones/funcionestp.php
@@ -1,59 +1,76 @@
 <?php
 function validacion(){
-$errores=[];
-$erroresNombre=esAlfabeticoYMinimoDeCaracteres($_POST["nombre"],"nombre",3);
-if($erroresNombre != null){
-  $errores["nombre"]=$erroresNombre;
-}
-$erroresNacionalidad=esAlfabeticoYMinimoDeCaracteres($_POST["nacionalidad"],"nacionalidad",5);
-if($erroresNacionalidad != null){
-  $errores["nombre"]=$erroresNacionalidad;
-}
-if(estaVacio($_POST["email"])){
-  $errores["email"]  = "Debe completar este campo <br>";
-}if (filter_var($_POST["email"],FILTER_VALIDATE_EMAIL)==false) {
+    $errores=[];
+    $erroresNombre=esAlfabeticoYMinimoDeCaracteres($_POST["nombre"],"nombre",5);
+    if($erroresNombre != null){
+      $errores["nombre"]=$erroresNombre;
+    }
+    if(estaVacio($_POST["email"])){
+      $errores["email"]="Debe completar este campo <br>";
+    }
+    if (filter_var($_POST["email"],FILTER_VALIDATE_EMAIL) == false) {
       $errores["email"]= "El mail no tiene el formato correcto <br>";
     }
-    if (estaVacio($_POST["fecha-nac"])==0) {
+    if (estaVacio($_POST["fecha-nac"])) {
       $errores["fecha-nac"]= "Debe completar una fecha de nacimiento <br>";
-    if(estaVacio($_POST["password"])){
-      $errores["password"]="Dejaste la contraseña vacia";
     }
-    if(estaVacio($_POST["confpassword"])){
-      $errores["confPassword"]="Dejaste la contraseña vacia";
+    $erroresClave=validar_pass($_POST["password"],$_POST["confPassword"]);
+    if($erroresClave != null){
+      $errores["password"]=$erroresClave;
     }
-    if (!estaVacio($_POST["password"])&&!estaVacio($_POST["confPassword"])&&$_POST["password"]!=$_POST["confPassword"]{
-      $errores["password"]="Las contraseñas no coinciden";
-    }
-    if (isset($_POST["pregunta-seguridad"])) {
+    if (isset($_POST["pregunta-seguridad"])==false ) {
       $errores["pregusnta-seguridad"]= "Debes elegir una pregunta <br>";
     }
     if(estaVacio($_POST["respuesta"])){
-      $errores["respuesta"]="Debe escribir una respuesta";
+      $errores["respuesta"]="Debe escribir una respuesta <br>";
     }
-    if (isset($_POST["genero"])) {
+    if (isset($_POST["genero"])==false) {
       $errores["genero"]= "Debes seleccionar una opcion <br>";
     }
-
-
-
-
     return $errores;
 }
-function estaVacio($campo){
-  if ($campo==""){
+function estaVacio($campo) {
+  if ($campo == "") {
     return true;
-  }else{
+  } else {
     return false;
   }
 }
-function esAlfabeticoYMinimoDeCaracteres($campo,$nombreCampo,$min){
-  if (estaVacio($campo)==0) {
+function esAlfabeticoYMinimoDeCaracteres($campo, $nombreCampo, $min){
+  if (estaVacio($campo)) {
     return "No llenaste el $nombreCampo <br>";
-  }else if(ctype_alpha($_POST["nombre"])==false){
-      return"el campo debe ser alfabetico";
-  }else if(strlen($_POST["nombre"]<3){
-    return"El $campo debe tener al menos $min letras";
+  } else if(strlen($campo) < $min){
+    return "El $nombreCampo debe tener al menos $min caracteres <br>";
+  } else if(ctype_alpha($campo) == false){
+    return "El $nombreCampo debe ser alfabetico <br>";
+  } else {
+    return null;
   }
 }
- ?>
+  function validar_pass($clave,$cpass){
+     if(strlen($clave) < 6){
+        $error_clave= "La $clave debe tener al menos 6 caracteres";
+     }else if(strlen($clave) > 16){
+        return "La $clave  no puede tener más de 16 caracteres";
+     }else if (!preg_match('`[a-z]`',$clave)){
+        return "La $clave debe tener al menos una letra minúscula";
+     }else if (!preg_match('`[A-Z]`',$clave)){
+        return "La $clave debe tener al menos una letra mayúscula";
+     }else if (!preg_match('`[0-9]`',$clave)){
+        return "La $clave debe tener al menos un caracter numérico";
+     } else if(!estaVacio($clave) && !estaVacio($cpass) && $clave != $cpass){
+       return"Las contraseñas no coinciden <br>";
+     }else{
+       return null;
+     }
+}
+function crearUsuario($usuario){
+  $usuarios=file_get_contents(usuarios.json);
+  $usuarios=json_decode($usuarios,true);
+
+  if($usuarios=== NULL){
+    $usuarios=[];
+  }
+  $usuarios[]=$usuario;
+  $usuarios=json_enconde($usuarios);
+}
--- register.php
+++ register.php
@@ -4,17 +4,19 @@ $validacionLogin=[];
 $nombreDefault="";
 $emailDefault="";
 $fechaNacDefault="";
-$nacionalidadDefault="";
 
 if ($_POST) {
   $validacionLogin=validacion();
+
   if(count($validacionLogin)==0){
+    $usuario = armarUsuario();
+    guardarUsuario($usuario);
+    guardarAvatar($usuario);
     header ("location:index.php");exit;
   }
   $nombreDefault=$_POST["nombre"];
   $emailDefault=$_POST["email"];
   $fechaNacDefault=$_POST["fecha-nac"];
-  $nacionalidadDefault=$_POST["nacionalidad"];
 }
  ?>
 <!DOCTYPE html>
@@ -24,7 +26,7 @@ if ($_POST) {
     <link href="https://fonts.googleapis.com/css?family=Acme|Cairo|Catamaran" rel="stylesheet">
     <meta name="viewport" content="width=device-width-initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <meta charset="utf-8">
     <title>Login</title>
   </head>
@@ -43,81 +45,72 @@ if ($_POST) {
 
     <section class="call-md-8 call-xs-12" "container">
       <div class="call.md-4 call-xs-12">
-        <ul style="color:red">
-          <?php
-            foreach ($validacionLogin as $error) {?>
-                <li><?php $validacion[$i] ?></li>
-          <?php }?>
-        </ul>
-      <form class="register" action="register.php" method="post">
-        <fieldset class="register">
-          <legend>Registrate</legend>
-          <p>
-            <?php if (isset($errores["nombre"])) :?>
-                  <input style="border: 1px solid red;" id="nombre" type="text" name="nombre" value= "" placeholder="Nombre y apellido">
-                  <p style="color:red; font-size:8px;"><?php $errores["nombre"] ?></p>
-            <?php else : ?>
-            <input id="nombre" type="text" name="nombre" value= "<?php $nombreDefault?>" >
-          <?php endif ?>
-          </p>
-          <p>
-            <?php if (isset($errores["email"])) :?>
-                  <input style="border: 1px solid red;" id="email" type="email" name="email" value= "" placeholder="Ingrese su email aqui...">
-                  <p style="color:red; font-size:8px;"><?php $errores["email"] ?></p>
-            <?php else : ?>
-            <input id="email" type="email" name="email" value= "<?php $emailDefault?>" >
-          <?php endif ?>
-          </p>
-          <p>
-            <?php if (isset($errores["fecha-nac"])) :?>
-                  <input style="border: 1px solid red;" id="fecha-nac" type="date" name="fecha-nac" value= "" placeholder="dd/mm/aaaa">
-                  <p style="color:red; font-size:8px;"><?php $errores["fecha-nac"] ?></p>
-            <?php else : ?>
-            <input id="fecha-nac" type="text" name="fecha-nac" value= "<?php $fechaNacDefault?>" >
-          <?php endif ?>
-          </p>
-          <p>
-            <?php if (isset($errores["nacionalidad"])) :?>
-                  <input style="border: 1px solid red;" id="nacionalidad" type="text" name="nacionalidad" value= "" placeholder="Ingrese su nacionalidad">
-                  <p style="color:red; font-size:8px;"><?php $errores["nacionbalidad"] ?></p>
-            <?php else : ?>
-            <input id="nacionalidad" type="text" name="nacionalidad" value= "<?php $nacionalidadDefault?>" >
-          <?php endif ?>
-          </p>
-          <p>
-            <input id="pass" type="password" name="password" value=""placeholder="Contraseña">
-          </p>
-          <p>
-            <input id="confPass" type="password" name="confPassword" value=""placeholder="confirmar Contraseña">
-          </p>
-          <p>
-          <select class="form-control" name="pregunta-seguridad">
-            <option value="null"class="hidden" selected disabled>Elegi una pregunta</option>
-            <option value="0">Nombre de tu primer mascota?</option>
-            <option value="1">Cual fue tu primer auto?</option>
-            <option value="2">Como se llamaba tu escuela primaria?</option>
-          </select>
-          <p>
-          <p>
-            <input id="respuesta" type="text" name="respuesta" value=""placeholder="Nombre y apellido">
-          </p>
-            <label >Genero</label>
-            <?php if(isset($_POST["genero"])&& $_POST["genero"]=="H"))?>
-            <input calss= "radio" type="radio" name="genero" value="H" checked>Hombre
-            <?php}else if{?>
-            <input calss= "radio" type="radio" name="genero" value="H"> Hombre
-          <?php } ?>
-          <?php if(isset($_POST["genero"] && $_POST["genero"]=="M"))?>
-          <input calss= "radio" type="radio" name="genero" value="M" checked>Mujer
-          <?php}else if{?>
-          <input calss= "radio" type="radio" name="genero" value="M">Mujer
-        <?php } ?>
-          </p>
-          <p class="button">
-            <button type="submit" name="button">Enviar</button>
-          </p>
-        </fieldset>
-      </form>
+        <form class="register" action="register.php" method="post">
+          <fieldset class="register">
+            <ul style="color:red; font-size:10px;text-alingn:right;">
+              <?php foreach ($validacionLogin as $error) :?>
+              <li><?= $error; ?></li>
+            <?php endforeach; ?>
+            </ul>
+            <legend>Registrate</legend>
+            <p>
+              <?php if (isset($validacionLogin["nombre"])) :?>
+                    <input style="border: 1px solid red;" id="nombre" type="text" name="nombre" value= "" placeholder="Nombre y apellido">
+                    <p style="color:red; font-size:8px;"><?php $validacionLogin["nombre"] ;?></p>
+              <?php else : ?>
+                    <input id="nombre" type="text" name="nombre" value= "<?php $nombreDefault;?>" placeholder="Nombre y apellido" >
+              <?php endif; ?>
+            </p>
+            <p>
+              <?php if (isset($validacionLogin["email"])) :?>
+                    <input style="border: 1px solid red;" type="email" name="email" value= "" placeholder="Ingrese su email aqui...">
+                    <p style="color:red; font-size:8px;"><?php $validacionLogin["email"] ;?></p>
+              <?php else : ?>
+                    <input id="email" type="email" name="email" value="<?php $emailDefault;?>" placeholder="Ingrese su email aqui...">
+              <?php endif; ?>
+            </p>
+            <p>
+              <?php if (isset($validacionLogin["fecha-nac"])) :?>
+                    <input style="border: 1px solid red;" id="fecha-nac" type="date" name="fecha-nac" value= "" placeholder="dd/mm/aaaa">
+                    <p style="color:red; font-size:8px;"><?php $validacionLogin["fecha-nac"]; ?></p>
+              <?php else : ?>
+                    <input id="fecha-nac" type="date" name="fecha-nac" value= "<?php $fechaNacDefault;?>" placeholder="fecha de nac dd/mm/aaaa">
+              <?php endif ;?>
+            </p>
+            <p>
+              <input id="pass" type="password" name="password" value=""placeholder="Contraseña">
+            </p>
+            <p>
+              <input id="confPass" type="password" name="confPassword" value=""placeholder="confirmar Contraseña">
+            </p>
+            <p>
+            <select style="width:240px" class="form-control" name="pregunta-seguridad">
+              <option value="null"class="hidden" selected disabled>Elegi una pregunta</option>
+              <option value="0">Nombre de tu primer mascota?</option>
+              <option value="1">Cual fue tu primer auto?</option>
+              <option value="2">Como se llamaba tu escuela primaria?</option>
+            </select>
+            <p>
+            <p>
+              <input id="respuesta" type="text" name="respuesta" value=""placeholder="">
+            </p>
+              <label >Genero</label>
+              <?php if(isset($_POST["genero"])&& $_POST["genero"]=="H"):?>
+              <input calss= "radio" type="radio" name="genero" value="H" checked>Hombre
+              <?php else :?>
+              <input calss= "radio" type="radio" name="genero" value="H">Hombre
+            <?php  endif; ?>
+            <?php if(isset($_POST["genero"])&& $_POST["genero"]=="M"):?>
+            <input calss= "radio" type="radio" name="genero" value="M" checked>Mujer
+          <?php else :?>
+            <input calss= "radio" type="radio" name="genero" value="M">Mujer
+          <?php endif ;?>
+            </p>
+            <p class="button">
+              <button type="submit" name="button">Enviar</button>
+            </p>
+          </fieldset>
+        </form>
       </div>
     </section>
     <footer>
